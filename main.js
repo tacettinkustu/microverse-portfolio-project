290a3b577d8475609b4577457e5bdf9e920c324f
tacettinkustu <tacettinkustu>
2021-06-22 09:38:52 +0300

    Add menu closing attributes

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.is-open');
+const closeMenu = document.querySelector('.close-menu')
+const listMenu = document.querySelectorAll('.nav-menu-link')
+const contact = document.querySelector('#contact-padding')
 
-function toggleMenu(e) {
+function toggleMenu() {
   if (navMenu.style.display === 'block') {
     navMenu.style.display = 'none';
   } else {
     navMenu.style.display = 'block';
   }
-  e.preventDefault();
 }
 
 hamburgerMenu.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click',toggleMenu);
